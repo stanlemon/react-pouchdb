c5de04e24e2044de877720c7b2793545f2d01153
Stan Lemon <stanlemon@users.noreply.github.com>
2020-11-09 21:14:28 -0500

    Switch to testing library

--- src/Database.test.tsx
+++ src/Database.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { render, screen } from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
 
 import { Database } from "./Database";
 
-test("<Database/> renders children", async () => {
+test("<Database/> renders children", async (): Promise<void> => {
   const text = "Hello World";
   render(
     <Database>
@@ -11,6 +11,6 @@ test("<Database/> renders children", async () => {
     </Database>
   );
 
-  const items = await screen.findAllByText(text)
-  expect(items).toHaveLength(1)
+  const items = await screen.findAllByText(text);
+  expect(items).toHaveLength(1);
 });
